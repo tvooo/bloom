4b37a980b69858f8b07b7fd3869cc0aad8b1514a
Tim von Oldenburg <tim@tvooo.de>
2022-01-02 18:54:31 +0100

    Fix DB path

--- server/database/db.ts
+++ server/database/db.ts
@@ -1,6 +1,6 @@
 import sqlite3 from 'sqlite3';
 
-const db = new sqlite3.Database('/Users/tim/Nextcloud/bloom/bloom-imported.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+const db = new sqlite3.Database(process.env.BLOOM_DB || 'bloom.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
     console.log('error opening db', err)
   });
 
