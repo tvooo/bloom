85abea68a0c10273c715a4fd4b34cfb1211cc112
Tim von Oldenburg <tim@tvooo.de>
2022-01-13 22:56:57 +0100

    Improve progress indicator somewhat

--- components/Pie.tsx
+++ components/Pie.tsx
@@ -15,16 +15,18 @@ const Circle = styled.circle<{ progress: number; color: string; size: number }>`
   }};
 `;
 
+const shrink = 0.15;
+
 const Pie = styled.circle<{ progress: number; color: string; size: number }>`
   stroke: ${(props) => props.color};
-  stroke-width: ${props => props.size * 0.4};
+  stroke-width: ${props => props.size * shrink * 2};
   fill: transparent;
-  r: ${props => props.size * 0.2}px;
+  r: ${props => props.size * shrink}px;
   cx: ${props => props.size * 0.5}px;
   cy: ${props => props.size * 0.5}px;
-  stroke-dasharray: ${props => 0.2 * props.size * 2 * Math.PI};
+  stroke-dasharray: ${props => shrink * props.size * 2 * Math.PI};
   stroke-dashoffset: ${(props) => {
-    const radius = 0.2 * props.size;
+    const radius = shrink * props.size;
     const circumference = radius * 2 * Math.PI;
     return circumference - (props.progress / 100) * circumference;
   }};
@@ -33,7 +35,6 @@ const Pie = styled.circle<{ progress: number; color: string; size: number }>`
 export const ProgressPie = ({ progress, size = 16 }: { progress: number; size?: number }) => {
   return (
     <svg width={`${size}px`} height={`${size}px`} style={{ transform: 'rotate(-90deg)' }}>
-      <Pie progress={100} color="var(--color-neutral-light)" size={size} />
       <Pie progress={progress} color="var(--color-project)" size={size} />
       <Circle progress={100} color="var(--color-project)" size={size} />
     </svg>
